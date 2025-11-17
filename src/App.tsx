5e96e925ae9944e55f6d707dd743c448da55656d
Jayson Bam <u23557894@tuks.co.za>
2025-11-17 09:56:12 +0200

    Fix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-[#121212]">
-      <h1 className="text-3xl font-semibold text-gray-900 dark:text-white">Hello, world!</h1>
+    <main>
+      <h1>Hello, world!</h1>
     </main>
   )
 }
